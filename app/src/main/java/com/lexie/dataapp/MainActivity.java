30906c902cdff069e9fd95ed837645c440540984
unknown <lxwaihenya@gmail.com>
2021-04-29 14:30:17 +0300

    second commit

--- app/src/main/java/com/lexie/dataapp/MainActivity.java
+++ app/src/main/java/com/lexie/dataapp/MainActivity.java
@@ -2,18 +2,29 @@ package com.lexie.dataapp;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
     private FirebaseAuth mAuth;
     private FirebaseUser currentUser;
+    private DrawerLayout drawerLayout;
+    private NavigationView navigationView;
+    private AppBarConfiguration appBarConfiguration;
+    private NavController navController;
 
 
     @Override
@@ -24,6 +35,19 @@ public class MainActivity extends AppCompatActivity {
 
         mAuth = FirebaseAuth.getInstance();
         currentUser = mAuth.getCurrentUser();
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.navigationView);
+        appBarConfiguration = new AppBarConfiguration.Builder(R.id.dataFragment,R.id.graphFragment)
+                .setDrawerLayout(drawerLayout)
+                .build();
+
+
+
+        navController = Navigation.findNavController(this,R.id.fragment);
+        NavigationUI.setupActionBarWithNavController(this,navController,appBarConfiguration);
+        NavigationUI.setupWithNavController(navigationView,navController);
+
 
 
 
@@ -44,6 +68,14 @@ public class MainActivity extends AppCompatActivity {
             default:
                 return super.onOptionsItemSelected(item);
         }
+
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        navController = Navigation.findNavController(this,R.id.fragment);
+        return NavigationUI.navigateUp(navController,appBarConfiguration)
+                || super.onSupportNavigateUp();
 
     }
 
